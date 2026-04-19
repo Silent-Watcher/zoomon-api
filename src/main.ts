ae6714ba4590129c31efe3c78f4158c6569104b2
Silent-Watcher <alitabatabaee20@gmail.com>
2026-04-19 11:11:26 +0330

    feat(app): added custom logger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { AppLogger } from "./logger/logger.service";
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule);
+	const app = await NestFactory.create(AppModule, {
+		bufferLogs: true,
+	});
+	app.useLogger(new AppLogger());
 	await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
