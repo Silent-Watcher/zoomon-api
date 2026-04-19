3e8f6c0c6f96a118b29cb9295a55097c1bca1140
Silent-Watcher <alitabatabaee20@gmail.com>
2026-04-19 11:36:45 +0330

    feat: added api Media type versioning feature

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { AppLogger } from "./logger/logger.service";
+import { VersioningType } from "@nestjs/common";
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule, {
@@ -8,6 +9,11 @@ async function bootstrap() {
 	});
 	app.useLogger(new AppLogger());
 	app.setGlobalPrefix("api");
+	app.enableVersioning({
+		type: VersioningType.MEDIA_TYPE,
+		key: "v=",
+		defaultVersion: "1",
+	});
 	await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
