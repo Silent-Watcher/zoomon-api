57855f899713b6e4b29b3b08d33586304d27e7f1
Silent-Watcher <alitabatabaee20@gmail.com>
2026-04-21 11:44:21 +0330

    feat(app): added helmet for adding security headers

--- src/common/helpers/app.helper.ts
+++ src/common/helpers/app.helper.ts
@@ -2,11 +2,13 @@ import { INestApplication } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import cookieParser from 'cookie-parser';
 import session from 'express-session';
+import helmet from 'helmet';
 
 export function registerGlobalMiddlewares(app: INestApplication): void {
 	const config = app.get(ConfigService);
 	const sessionOptions = config.get<session.SessionOptions>('session');
 
+	app.use(helmet());
 	app.use(cookieParser());
 	app.use(session(sessionOptions));
 }
